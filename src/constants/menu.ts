2da09819515a79a996ba5977e38344622e37a9b1
Simone Rosani <rsn.smn@gmail.com>
2025-03-04 20:31:55 +0100

    feat: add listen link

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -5,7 +5,7 @@ interface MenuItem {
 
 export const menuItems: Array<MenuItem> = [
     { name: "COMPRA", url: "/shop" },
-    { name: "ASCOLTA", url: "/listen" },
+    { name: "ASCOLTA", url: "https://links.altafonte.com/a3ya5bo?fbclid=PAZXh0bgNhZW0CMTEAAaZkwyZsHFKa5S2foxj7zgNe6tKdLyjpqkRIt5eJTIKXH9kyG4yRpz1Y3Rs_aem_L-jq9jTz1y9blekE-L-5zg" },
     { name: "MERCH", url: "/merch" },
     { name: "TOUR", url: "/contact" }
 ]
