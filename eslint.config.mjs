58ca3054bf45d654829aedad0a96c61da260cebd
Simone Rosani <rsn.smn@gmail.com>
2025-03-07 14:56:20 +0100

    chore: fix typo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
-    ruls: {
+    rules: {
       "@typescript-eslint/no-unused-vars": "off"
     }
   })
