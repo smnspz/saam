a2bdbe6b4a7cf13f93c4d08c9d6da7680b21372b
Simone Rosani <rsn.smn@gmail.com>
2025-03-19 17:00:35 +0100

    temporarily remove merch link

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -4,11 +4,13 @@ interface MenuItem {
 }
 
 export const menuItems: Array<MenuItem> = [
-  { name: "COMPRA", url: "/merch/vinile" },
+  {
+    name: "COMPRA",
+    url: "https://nontiseguorecords.bigcartel.com/product/saam-per-ogni-caduta-una-terra-amata",
+  },
   {
     name: "ASCOLTA",
     url: "https://links.altafonte.com/a3ya5bo?fbclid=PAZXh0bgNhZW0CMTEAAaZkwyZsHFKa5S2foxj7zgNe6tKdLyjpqkRIt5eJTIKXH9kyG4yRpz1Y3Rs_aem_L-jq9jTz1y9blekE-L-5zg",
   },
-  { name: "MERCH", url: "/merch" },
   { name: "TOUR", url: "/tour" },
 ];
